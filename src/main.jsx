48418afaa85aedad60a8d0bb92e6eabab1d8e545
Somaditya Biswas <158031340+SBiswas765@users.noreply.github.com>
2025-11-30 18:30:00 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-// empty placeholder (you will replace later)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
